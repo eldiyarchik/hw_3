071f49d618ac630482ef86c34569c64265b6d85a
eldiyarchik <eldiarbaurzanov@gmail.com>
2022-12-08 19:21:02 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] nums = {0.53, -1.2, 2.23, 2.31, 9.6, -4.21, 0.43, 3.2, -5.32, -4.52, 1.4, 7.63, 10.1, -2.7, 6.1};
+        for (double x : nums) {
+
+        }
+    }
+}
